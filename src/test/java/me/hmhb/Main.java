2094bad58bac80b191511a57caabdd50f98ad2c6
twj <944725820@qq.com>
2026-04-13 21:35:02 +0800

    Revert "提交1"
    This reverts commit e025f8ed3063c896bf1a46ce6bc16f5444d11818.

---
+++ src/test/java/me/hmhb/Main.java
@@ -0,0 +1,14 @@
+package me.hmhb;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+
+        List<String> list = Arrays.asList("a", "b", "c");
+        list.addFirst("d");
+        list.addLast("e");
+        
+    }
+}
